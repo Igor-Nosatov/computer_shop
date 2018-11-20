137aaf1c9c5ff91579668579ab484986418c562a
Igor-Nosatov <sinevor13@gmail.com>
2018-11-20 12:34:48 +0200

    start to make multi auth admin and client

---
+++ app/Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Client extends Model
+{
+    use Notifiable;
+
+    protected $guard = 'client';
+
+    protected $fillable = [
+    	'name', 'email', 'password'];
+
+    protected $hidden = ['password',
+    'remember_token' 
+    ];
+}
